7e526a9be83e81ff80f40b8dcb74d46f2d547ca2
Stijn van_der_Made <2120804@talnet.nl>
2020-02-06 10:51:55 +0100

    Uitwerking van de exercise

---
+++ script.js
@@ -0,0 +1,16 @@
+const pi = 3.14;
+
+
+function calculate() {
+    let nummer = document.getElementById('number').value;
+    document.getElementById('omtrek').innerHTML = nummer * pi;
+    document.getElementById('oppervlakte').innerHTML = nummer * nummer * pi * 0.25;
+    
+}
+
+function printdate() {
+    var d = new Date();
+    document.getElementById("datum").innerHTML = d;
+}
+
+var t = setInterval(printdate,1000);
